fa66eac26d4ba54917e51eecdbfb65c6e434a255
DS23666X <diasgsputra@gmail.com>
2024-03-01 20:48:38 +0700

    [Swagger] fixing swagger

--- swagger.js
+++ swagger.js
@@ -13,7 +13,7 @@ const options = {
         },
         servers: [
             {
-                url: 'https://loyalty-management-seven.vercel.app',
+                url: '0.0.0.0',
                 description: 'Development server',
             },
         ],
